cf03617f4095aad35b38e38846a6f295224cfbb9
jbphet <john.blanco@colorado.edu>
2019-04-11 14:55:29 -0600

    added check box for tick marks

--- js/generic/view/NLIGenericScreenView.js
+++ js/generic/view/NLIGenericScreenView.js
@@ -9,17 +9,25 @@ define( function( require ) {
   'use strict';
 
   // modules
+  const Checkbox = require( 'SUN/Checkbox' );
   const Color = require( 'SCENERY/util/Color' );
   const numberLineIntegers = require( 'NUMBER_LINE_INTEGERS/numberLineIntegers' );
   const PointControllerNode = require( 'NUMBER_LINE_INTEGERS/common/view/PointControllerNode' );
   const NLIConstants = require( 'NUMBER_LINE_INTEGERS/common/NLIConstants' );
   const NumberLineNode = require( 'NUMBER_LINE_INTEGERS/common/view/NumberLineNode' );
+  const PhetFont = require( 'SCENERY_PHET/PhetFont' );
   const Rectangle = require( 'SCENERY/nodes/Rectangle' );
   const ResetAllButton = require( 'SCENERY_PHET/buttons/ResetAllButton' );
   const ScreenView = require( 'JOIST/ScreenView' );
+  const Text = require( 'SCENERY/nodes/Text' );
+  const Vector2 = require( 'DOT/Vector2' );
 
   // constants
   const POINT_CONTROLLER_COLOR_LIST = [ new Color( 'blue' ), new Color( 'magenta' ), new Color( 'orange' ) ];
+  const CHECK_BOX_FONT = new PhetFont( 20 );
+
+  // strings
+  const tickMarksString = require( 'string!NUMBER_LINE_INTEGERS/tickMarks' );
 
   class NLIGenericScreenView extends ScreenView {
 
@@ -29,6 +37,17 @@ define( function( require ) {
     constructor( model ) {
 
       super( { layoutBounds: NLIConstants.NLI_BOUNDS } );
+
+      // add the check boxes that will control the number line's presentation
+      const checkboxAreaUpperLeft = new Vector2( this.layoutBounds.maxX - 300, this.layoutBounds.minY + 10 );
+      this.addChild( new Checkbox(
+        new Text( tickMarksString, { font: CHECK_BOX_FONT } ),
+        model.numberLine.tickMarksVisibleProperty,
+        {
+          left: checkboxAreaUpperLeft.x,
+          top: checkboxAreaUpperLeft.y
+        }
+      ) );
 
       // NOTE: There is no model-view transform for this sim.  Model and view space use the same coordinate system.
 
