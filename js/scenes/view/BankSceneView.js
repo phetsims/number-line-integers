14e8fc64a9fd89a051abac729e1a77bfd70db6ad
SaurabhTotey <SaurabhTotey@gmail.com>
2019-08-14 11:48:58 -0600

    Started pulling out PiggyBankNode into separate file

--- js/scenes/view/BankSceneView.js
+++ js/scenes/view/BankSceneView.js
@@ -4,6 +4,7 @@
  * view for the "Bank" scene
  *
  * @author John Blanco (PhET Interactive Simulations)
+ * @author Saurabh Totey
  */
 define( require => {
   'use strict';
@@ -14,15 +15,12 @@ define( require => {
   const BankPointControllerNode = require( 'NUMBER_LINE_INTEGERS/scenes/view/BankPointControllerNode' );
   const Dimension2 = require( 'DOT/Dimension2' );
   const HBox = require( 'SCENERY/nodes/HBox' );
-  const Image = require( 'SCENERY/nodes/Image' );
   const Node = require( 'SCENERY/nodes/Node' );
   const numberLineIntegers = require( 'NUMBER_LINE_INTEGERS/numberLineIntegers' );
-  const Path = require( 'SCENERY/nodes/Path' );
   const PhetFont = require( 'SCENERY_PHET/PhetFont' );
-  const piggyBankShapes = require( 'NUMBER_LINE_INTEGERS/scenes/view/piggyBankShapes' );
+  const PiggyBankNode = require( 'NUMBER_LINE_INTEGERS/scenes/view/PiggyBankNode' );
   const SceneView = require( 'NUMBER_LINE_INTEGERS/scenes/view/SceneView' );
   const Text = require( 'SCENERY/nodes/Text' );
-  const Vector2 = require( 'DOT/Vector2' );
 
   // constants
   const NUMBER_LINE_LABEL_FONT = new PhetFont( { size: 18, weight: 'bold' } );
@@ -125,28 +123,6 @@ define( require => {
         switchSize: new Dimension2( 50, 15 ),
         setEnabled: () => {}
       }, options );
-
-      class PiggyBankNode extends Node {
-
-        // TODO: lots of duplication between here and BankPointControllerNode; this class could probably be pulled out into its own file
-        constructor( imageSource ) {
-          const piggyBankOutlineNode = new Path( piggyBankShapes.MEDIUM_PIGGY_BANK_SHAPE, {
-            fill: 'rgba( 0, 0, 0, 0 )', // transparent to start so it has size
-            lineWidth: 0,
-            center: Vector2.ZERO
-          } );
-          const overlayImage = new Image( imageSource, { opacity: 0.8 } );
-          overlayImage.setScaleMagnitude( piggyBankOutlineNode.width / overlayImage.width );
-          overlayImage.center = Vector2.ZERO;
-          super( { children: [ piggyBankOutlineNode, overlayImage ] } );
-          this.outline = piggyBankOutlineNode;
-        }
-
-        setHasFill( hasFill ) {
-          this.outline.fill = `rgba( 255, 255, 255, ${ hasFill ? 255 : 0 } )`;
-        }
-
-      }
 
       // TODO: these are not named well: single means on the left side to allow user to control single piggy bank
       //  double means that user can control 2 piggy banks
---
+++ js/scenes/view/PiggyBankNode.js
@@ -0,0 +1,46 @@
+// Copyright 2019, University of Colorado Boulder
+
+/**
+ * A node that just represents a piggy bank
+ *
+ * @author John Blanco (PhET Interactive Simulations)
+ * @author Saurabh Totey
+ */
+define( require => {
+  'use strict';
+
+  // modules
+  const Image = require( 'SCENERY/nodes/Image' );
+  const Node = require( 'SCENERY/nodes/Node' );
+  const numberLineIntegers = require( 'NUMBER_LINE_INTEGERS/numberLineIntegers' );
+  const Path = require( 'SCENERY/nodes/Path' );
+  const piggyBankShapes = require( 'NUMBER_LINE_INTEGERS/scenes/view/piggyBankShapes' );
+  const Vector2 = require( 'DOT/Vector2' );
+
+  class PiggyBankNode extends Node {
+
+    /**
+     * @param {Image} imageSource
+     */
+    constructor( imageSource ) {
+      const piggyBankOutlineNode = new Path( piggyBankShapes.MEDIUM_PIGGY_BANK_SHAPE, {
+        fill: 'rgba( 0, 0, 0, 0 )', // transparent to start so it has size
+        lineWidth: 0,
+        center: Vector2.ZERO
+      } );
+      const overlayImage = new Image( imageSource, { opacity: 0.8 } );
+      overlayImage.setScaleMagnitude( piggyBankOutlineNode.width / overlayImage.width );
+      overlayImage.center = Vector2.ZERO;
+      super( { children: [ piggyBankOutlineNode, overlayImage ] } );
+      this.outline = piggyBankOutlineNode;
+    }
+
+    setHasFill( hasFill ) {
+      this.outline.fill = `rgba( 255, 255, 255, ${ hasFill ? 255 : 0 } )`;
+    }
+
+  }
+
+  return numberLineIntegers.register( 'PiggyBankNode', PiggyBankNode );
+
+} );
