2fd733e8a7a323f4c524014b3c5f167adb454dbb
jbphet <john.blanco@colorado.edu>
2019-08-19 15:10:21 -0600

    added some of the scripts that are needed to obtain and process the data for the "Temperature" scene

---
+++ assets/scripts/get-nli-temperature-data.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import cdsapi
+c = cdsapi.Client()
+c.retrieve(
+  'reanalysis-era5-single-levels-monthly-means',
+  {
+    'product_type':'monthly_averaged_reanalysis',
+    'variable':'temperature',
+    'grid':[1.0,1.0],
+    'year':'2018',
+    'month':'01',
+    'time':'00:00',
+    'format':'netcdf'
+  },
+  "download.nc"
+)
