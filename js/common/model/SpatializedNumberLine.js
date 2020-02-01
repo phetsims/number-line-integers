42a2338d2ea20fc6d932ca3cff2df73232bf0392
phet-dev <phetadmin@gmail.com>
2020-02-01 03:42:38 -0700

    update copyright dates from daily grunt work

--- js/common/model/SpatializedNumberLine.js
+++ js/common/model/SpatializedNumberLine.js
@@ -1,4 +1,4 @@
-// Copyright 2019, University of Colorado Boulder
+// Copyright 2019-2020, University of Colorado Boulder
 
 /**
  * SpatializedNumberLine is a model of a number line that is projected into 2D space.  It also tracks a number of other
