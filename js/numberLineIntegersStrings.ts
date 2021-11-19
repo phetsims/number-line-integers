f99379797388ce6ff76d1d0f8c618c1904b0cdd2
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:35 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/numberLineIntegersStrings.ts
@@ -0,0 +1,64 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import numberLineIntegers from './numberLineIntegers.js';
+
+type StringsType = {
+  'number-line-integers': {
+    'title': string
+  },
+  'screen': {
+    'explore': string,
+    'generic': string
+  },
+  'absoluteValue': string,
+  'balance': string,
+  'comparisonStatement': string,
+  'elevation': string,
+  'labels': string,
+  'seaLevel': string,
+  'amountBelowSeaLevel': string,
+  'amountAboveSeaLevel': string,
+  'debtAmount': string,
+  'balanceAmount': string,
+  'negativeTemperatureAmount': string,
+  'positiveTemperatureAmount': string,
+  'zeroTemperatureAmount': string,
+  'elevationAmount': string,
+  'moneyAmount': string,
+  'currencyUnits': string,
+  'temperatureAmountCelsius': string,
+  'temperatureAmountFahrenheit': string,
+  'temperatureLabelCelsius': string,
+  'temperatureLabelFahrenheit': string,
+  'numberLine': string,
+  'opposite': string,
+  'temperature': string,
+  'tickMarks': string,
+  'thermometerALabel': string,
+  'thermometerBLabel': string,
+  'thermometerCLabel': string,
+  'temperatureMapCaption': string,
+  'january': string,
+  'february': string,
+  'march': string,
+  'april': string,
+  'may': string,
+  'june': string,
+  'july': string,
+  'august': string,
+  'september': string,
+  'october': string,
+  'november': string,
+  'december': string
+};
+
+const numberLineIntegersStrings = getStringModule( 'NUMBER_LINE_INTEGERS' ) as StringsType;
+
+numberLineIntegers.register( 'numberLineIntegersStrings', numberLineIntegersStrings );
+
+export default numberLineIntegersStrings;
