b2b2c9730a0115c6c3777f4432fb6d0ea0e3c401
Sam Reid <reids@colorado.edu>
2022-05-07 09:34:05 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/numberLineIntegersStrings.ts
+++ js/numberLineIntegersStrings.ts
@@ -9,52 +9,52 @@ import numberLineIntegers from './numberLineIntegers.js';
 
 type StringsType = {
   'number-line-integers': {
-    'title': string
-  },
+    'title': string;
+  };
   'screen': {
-    'explore': string,
-    'generic': string
-  },
-  'absoluteValue': string,
-  'balance': string,
-  'comparisonStatement': string,
-  'elevation': string,
-  'labels': string,
-  'seaLevel': string,
-  'amountBelowSeaLevel': string,
-  'amountAboveSeaLevel': string,
-  'debtAmount': string,
-  'balanceAmount': string,
-  'negativeTemperatureAmount': string,
-  'positiveTemperatureAmount': string,
-  'zeroTemperatureAmount': string,
-  'elevationAmount': string,
-  'moneyAmount': string,
-  'currencyUnits': string,
-  'temperatureAmountCelsius': string,
-  'temperatureAmountFahrenheit': string,
-  'temperatureLabelCelsius': string,
-  'temperatureLabelFahrenheit': string,
-  'numberLine': string,
-  'opposite': string,
-  'temperature': string,
-  'tickMarks': string,
-  'thermometerALabel': string,
-  'thermometerBLabel': string,
-  'thermometerCLabel': string,
-  'temperatureMapCaption': string,
-  'january': string,
-  'february': string,
-  'march': string,
-  'april': string,
-  'may': string,
-  'june': string,
-  'july': string,
-  'august': string,
-  'september': string,
-  'october': string,
-  'november': string,
-  'december': string
+    'explore': string;
+    'generic': string;
+  };
+  'absoluteValue': string;
+  'balance': string;
+  'comparisonStatement': string;
+  'elevation': string;
+  'labels': string;
+  'seaLevel': string;
+  'amountBelowSeaLevel': string;
+  'amountAboveSeaLevel': string;
+  'debtAmount': string;
+  'balanceAmount': string;
+  'negativeTemperatureAmount': string;
+  'positiveTemperatureAmount': string;
+  'zeroTemperatureAmount': string;
+  'elevationAmount': string;
+  'moneyAmount': string;
+  'currencyUnits': string;
+  'temperatureAmountCelsius': string;
+  'temperatureAmountFahrenheit': string;
+  'temperatureLabelCelsius': string;
+  'temperatureLabelFahrenheit': string;
+  'numberLine': string;
+  'opposite': string;
+  'temperature': string;
+  'tickMarks': string;
+  'thermometerALabel': string;
+  'thermometerBLabel': string;
+  'thermometerCLabel': string;
+  'temperatureMapCaption': string;
+  'january': string;
+  'february': string;
+  'march': string;
+  'april': string;
+  'may': string;
+  'june': string;
+  'july': string;
+  'august': string;
+  'september': string;
+  'october': string;
+  'november': string;
+  'december': string;
 };
 
 const numberLineIntegersStrings = getStringModule( 'NUMBER_LINE_INTEGERS' ) as StringsType;
