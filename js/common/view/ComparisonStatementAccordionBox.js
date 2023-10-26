1d9972ddc7a924de18ae9f23f09257920a4f0036
phet-dev <phetadmin@gmail.com>
2023-10-26 04:13:19 -0600

    update copyright dates from daily grunt work

--- js/common/view/ComparisonStatementAccordionBox.js
+++ js/common/view/ComparisonStatementAccordionBox.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2022, University of Colorado Boulder
+// Copyright 2019-2023, University of Colorado Boulder
 
 /**
  * ComparisonStatementAccordionBox is an accordion box that contains a "comparison statement", which is a mathematical
