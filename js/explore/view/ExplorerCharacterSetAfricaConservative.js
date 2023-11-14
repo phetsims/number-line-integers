0dce3aa12c4bf51f71eb7e2f56b3b9057c0b308e
Marla Schulz <marlaa.schulz@gmail.com>
2023-11-14 17:31:29 -0600

    Match screen icons to characters, see: https://github.com/phetsims/number-line-integers/issues/112

--- js/explore/view/ExplorerCharacterSetAfricaConservative.js
+++ js/explore/view/ExplorerCharacterSetAfricaConservative.js
@@ -8,11 +8,11 @@
 
 
 import RegionAndCultureHeadshotIcon from '../../../../joist/js/preferences/RegionAndCultureHeadshotIcon.js';
+import exploreScreenHome_png from '../../../images/africa-conservative/exploreScreenHome_png.js';
+import exploreScreenNav_png from '../../../images/africa-conservative/exploreScreenNav_png.js';
 import girlHiking_png from '../../../images/africa-conservative/girlHiking_png.js';
 import girlInAir_png from '../../../images/africa-conservative/girlInAir_png.js';
 import girlInWater_png from '../../../images/africa-conservative/girlInWater_png.js';
-import exploreScreenHome_png from '../../../images/africa/exploreScreenHome_png.js';
-import exploreScreenNav_png from '../../../images/africa/exploreScreenNav_png.js';
 import { AFRICA_MODEST_QUERY_VALUE } from '../../common/NLIQueryParameters.js';
 import NumberLineIntegersStrings from '../../NumberLineIntegersStrings.js';
 import ExplorerCharacterSet from './ExplorerCharacterSet.js';
